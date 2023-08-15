fbacb2ef4c7bb272d0e34fc119a34a1a93820e17
Tsatsov <ivaylo.tsatsov@gmail.com>
2023-08-16 00:49:38 +0300

    Created a Dto class and a Mapper for Card to serve the presentation layer.

---
+++ src/main/java/com/company/web/wallet/helpers/CardMapper.java
@@ -0,0 +1,39 @@
+package com.company.web.wallet.helpers;
+
+import com.company.web.wallet.models.Card;
+import com.company.web.wallet.models.CardDto;
+import com.company.web.wallet.models.User;
+import com.company.web.wallet.services.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+
+@Component
+public class CardMapper {
+    private final CardService service;
+
+    @Autowired
+    public CardMapper(CardService service) {
+        this.service = service;
+    }
+
+    public Card createCardDto(CardDto cardDto, User user) {
+        Card newCard = new Card();
+        newCard.setCardHolder(user);
+        newCard.setCardNumber(cardDto.getCardNumber());
+        newCard.setStatusDeleted(0);
+        newCard.setCheckNumber(cardDto.getCheckNumber());
+        newCard.setExpirationDate(cardDto.getExpirationDate());
+        newCard.setTransactionsSet(new HashSet<>());
+        return newCard;
+    }
+
+    public Card updateCardDto(int id, CardDto cardDto) {
+        Card existingCard = service.get(id);
+        existingCard.setExpirationDate(cardDto.getExpirationDate());
+        existingCard.setCardNumber(cardDto.getCardNumber());
+        existingCard.setCheckNumber(cardDto.getCheckNumber());
+        return existingCard;
+    }
+}
