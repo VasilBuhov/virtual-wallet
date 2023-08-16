dab09fa52bf2576f4435f54928c3931fefb42cc1
VasilBuhov1 <kp.buhov@gmail.com>
2023-08-16 11:31:32 +0300

    TransationRestController update

--- src/main/java/com/company/web/wallet/controllers/RestController/TransactionRestController.java
+++ src/main/java/com/company/web/wallet/controllers/RestController/TransactionRestController.java
@@ -1,7 +1,10 @@
 package com.company.web.wallet.controllers.RestController;
 
+import com.company.web.wallet.exceptions.AuthorizationException;
+import com.company.web.wallet.exceptions.BlockedUserException;
+import com.company.web.wallet.exceptions.EntityDeletedException;
+import com.company.web.wallet.exceptions.EntityNotFoundException;
 import com.company.web.wallet.helpers.AuthenticationHelper;
-
 import com.company.web.wallet.helpers.TransactionMapper;
 import com.company.web.wallet.models.Transaction;
 import com.company.web.wallet.models.TransactionDto;
@@ -12,7 +15,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -22,7 +27,6 @@ public class TransactionRestController {
     private final TransactionMapper transactionMapper;
     private final AuthenticationHelper authenticationHelper;
 
-
     @Autowired
     public TransactionRestController(TransactionService transactionService,
                                      TransactionMapper transactionMapper,
@@ -30,40 +34,69 @@ public class TransactionRestController {
         this.transactionService = transactionService;
         this.transactionMapper = transactionMapper;
         this.authenticationHelper = authenticationHelper;
-
     }
 
     @GetMapping
-    public ResponseEntity<List<TransactionDto>> getAllTransactions() {
-
-        List<Transaction> transactions = transactionService.getAllTransactions();
-        List<TransactionDto> transactionDtos = transactionMapper.toDtoList(transactions);
-        return new ResponseEntity<>(transactionDtos, HttpStatus.OK);
+    public List<TransactionDto> getAllTransactions(@RequestHeader HttpHeaders httpHeaders) {
+        try {
+            authenticationHelper.tryGetUser(httpHeaders);
+            List<Transaction> transactions = transactionService.getAllTransactions();
+            return transactionMapper.toDtoList(transactions);
+        } catch (AuthorizationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        } catch (EntityNotFoundException | EntityDeletedException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        } catch (BlockedUserException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<TransactionDto> getTransactionById(@PathVariable Long id) {
-
-        Transaction transaction = transactionService.getTransactionById(id);
-        TransactionDto transactionDto = transactionMapper.toDto(transaction);
-        return new ResponseEntity<>(transactionDto, HttpStatus.OK);
-    }
-
-    @PostMapping
-    public ResponseEntity<TransactionDto> createTransaction(HttpHeaders headers,
-                                                            @RequestBody TransactionDto transactionDto) {
-        User currentUser = authenticationHelper.tryGetUser(headers);
-        Transaction transaction = transactionMapper.fromDto(transactionDto);
-        transaction.setSender(currentUser);
-        transactionService.createTransaction(transaction);
-        TransactionDto createdTransactionDto = transactionMapper.toDto(transaction);
-        return new ResponseEntity<>(createdTransactionDto, HttpStatus.CREATED);
+    public TransactionDto getTransactionById(@PathVariable Long id, @RequestHeader HttpHeaders httpHeaders) {
+        try {
+            authenticationHelper.tryGetUser(httpHeaders);
+            Transaction transaction = transactionService.getTransactionById(id);
+            return transactionMapper.toDto(transaction);
+        } catch (AuthorizationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        } catch (EntityNotFoundException | EntityDeletedException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        } catch (BlockedUserException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTransaction(@PathVariable Long id) {
-
-        transactionService.deleteTransaction(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<Void> deleteTransaction(@PathVariable Long id, @RequestHeader HttpHeaders httpHeaders) {
+        try {
+            authenticationHelper.tryGetUser(httpHeaders);
+            transactionService.deleteTransaction(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (AuthorizationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        } catch (EntityNotFoundException | EntityDeletedException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        } catch (BlockedUserException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        }
     }
+
+    @PostMapping("/create")
+    public TransactionDto createTransaction(@RequestHeader HttpHeaders httpHeaders,
+                                            @Valid @RequestBody TransactionDto transactionDto) {
+        try {
+            User currentUser = authenticationHelper.tryGetUser(httpHeaders);
+            Transaction transaction = transactionMapper.fromDto(transactionDto);
+            transaction.setSender(currentUser);
+            transactionService.createTransaction(transaction);
+            return transactionMapper.toDto(transaction);
+        } catch (AuthorizationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        } catch (BlockedUserException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        }
+    }
+
+
+
 }
