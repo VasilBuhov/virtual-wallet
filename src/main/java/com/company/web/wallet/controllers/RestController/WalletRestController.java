edcfef30fc87f6634dd40afd4df64ed5a063c7f5
Tsatsov <ivaylo.tsatsov@gmail.com>
2023-08-30 23:38:22 +0300

    Created a Dummy Bank API, whose purpose is to simulate a real bank transfer, through consumption of external API.

--- src/main/java/com/company/web/wallet/controllers/RestController/WalletRestController.java
+++ src/main/java/com/company/web/wallet/controllers/RestController/WalletRestController.java
@@ -21,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,7 +31,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
