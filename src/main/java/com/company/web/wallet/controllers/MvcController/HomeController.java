b22211e7eed29ceb76aa4aeaaf36f3b8070dca5d
ARCH <minus273dot15@gmail.com>
2023-09-03 00:51:57 +0300

    Front end update: FAQ/Team/Terms/AboutUs

--- src/main/java/com/company/web/wallet/controllers/MvcController/HomeController.java
+++ src/main/java/com/company/web/wallet/controllers/MvcController/HomeController.java
@@ -31,4 +31,36 @@ public class HomeController {
 
         return "index";
     }
+
+    @GetMapping("/team")
+    public String showTeamPage(Model model) {
+        List<String> imageFilenames = Arrays.asList(
+                "team_ceo.jpg", "team_coo.jpg",
+                "team_finance_manager.jpg", "team_hr.jpg",
+                "team_accountant.jpg", "team_audit.jpg",
+                "team_sales.jpg", "team_secretory.jpg",
+                "team_developer.jpg", "team_it.jpg",
+                "team_marketing.jpg", "team_supply_chain.jpg",
+                "team_strategy_head.jpg",
+                "team_media_planner.jpg", "team_intern.jpg",
+                "team_copywriter.jpg", "team_creative_director.jpg",
+                "team_art_director.jpg", "team_account_executive.jpg");
+        model.addAttribute("imageFilenames", imageFilenames);
+        return "site/team";
+    }
+
+    @GetMapping("/terms")
+    public String showTermsPage() {
+        return "site/terms";
+    }
+
+    @GetMapping("/about")
+    public String showAboutUsPage() {
+        return "site/about_us";
+    }
+
+    @GetMapping("/faq")
+    public String showFAQPage() {
+        return "site/faq";
+    }
 }
